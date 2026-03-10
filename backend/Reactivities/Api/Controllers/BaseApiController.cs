dfd611e6212b9f99a4e09c815aa1de7c47e3490c
djquangsumi@gmail.com <djquangsumi@gmail.com>
2026-03-10 14:01:51 +0700

    Seed Data. Test activities endpoint

---
+++ backend/Reactivities/Api/Controllers/BaseApiController.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+}
+
+public class ActivitiesController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+        => await context.Activities.ToListAsync();
+}
